0ddc5459edb85b792890afdbfaa4d45807fad28f
felix <felix@roobodeMacBook-Pro.local>
2020-04-11 11:53:24 +0800

    init

---
+++ Example/Pods/Target Support Files/Pods-StpOCSDK_Tests/Pods-StpOCSDK_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_StpOCSDK_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_StpOCSDK_Tests
+@end
---
+++ Example/StpOCSDK/STPViewController.m
@@ -0,0 +1,29 @@
+//
+//  STPViewController.m
+//  StpOCSDK
+//
+//  Created by aie-felix on 04/10/2020.
+//  Copyright (c) 2020 aie-felix. All rights reserved.
+//
+
+#import "STPViewController.h"
+
+@interface STPViewController ()
+
+@end
+
+@implementation STPViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
